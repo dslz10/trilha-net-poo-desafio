fe455e3f06b2cb2a23a9334f7d9be0c24fe6a412
Diego Selzzo <diegoselzzo@gmail.com>
2022-09-26 15:21:23 -0300

    Implementação do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Nokia aparelhoNokia = new Nokia("+5511996325682", "A25", "Numero imei", 64);
+aparelhoNokia.Ligar();
+aparelhoNokia.InstalarAplicativo("Whatsapp");
+aparelhoNokia.InstalarAplicativo("Duolingo");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone aparelhoIphone = new Iphone("+5511996325600", "13x", "Numero imei", 128);
+aparelhoIphone.Ligar();
+aparelhoIphone.InstalarAplicativo("Facebook");
+aparelhoIphone.InstalarAplicativo("Instagram");
